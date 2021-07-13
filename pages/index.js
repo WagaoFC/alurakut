b449ddf66527346a98776b1ae51e3b7e5d515149
WagaoFC <wagnerferreira_07@hotmail.com>
2021-07-13 12:40:16 -0300

    finalizando home

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,61 @@
-import styled from 'styled-components'
+import MainGrid from '../src/components/MainGrid'
+import Box from '../src/components/Box'
+import { AlurakutMenu, OrkutNostalgicIconSet } from '../src/lib/AlurakutCommons'
+import { ProfileRelationsBoxWrapper } from '../src/components/ProfileRelations'
 
-// const Title = styled.h1`
-//   font-size: 50px;
-//   color: ${({ theme }) => theme.colors.primary};
-// `
-
-const Box = styled.div`
-  background: #FFF;
-  border-radius: 8px;
-`;
+function ProfileSidebar(props) {
+  return(
+    <Box>
+      <img src={ `https://github.com/${props.gitHubUser}.png` } style={{borderRadius:'8px'}}/>
+    </Box>
+  )
+}
 
 export default function Home() {
+
+  const gitHubUser = 'wagaofc';
+  const myFriends = ['justin1508','silasdearaujo', 'cristian-augusto', 'kelvyncosta', 'igorlacerdak', 'marceloromeiro'];
+
   return (
-    <main>
-      <Box>
-        Imagem
-      </Box>
-      <Box>
-        Bem vindo
-      </Box>
-      <Box>
-        Comunidades
-      </Box>
-    </main>
+    <>
+      <AlurakutMenu />
+      <MainGrid>
+        <div className="profileArea" style={{ gridArea: 'profileArea' }}>
+          <ProfileSidebar gitHubUser={gitHubUser}/>
+        </div>
+        <div className="welcomeArea" style={{ gridArea: 'welcomeArea' }}>
+          <Box>
+            <h1 className="Title">
+              Bem vindo(a)
+            </h1>
+
+            <OrkutNostalgicIconSet />
+
+          </Box>
+        </div>
+        <div className="profileRelationsArea" style={{ gridArea: 'profileRelationsArea' }}>
+          <ProfileRelationsBoxWrapper>
+            <h2 className="smallTitle">
+              Meus Amigos ({myFriends.length})
+            </h2>
+            <ul>
+              {myFriends.map((friend) => {
+                return(
+                  <li>
+                    <a href={`/users/${friend}`} key={friend}>
+                      <img src={`https://github.com/${friend}.png`} />
+                      <span>{friend}</span>
+                    </a>
+                  </li>
+                )
+              })}
+            </ul>
+          </ProfileRelationsBoxWrapper>
+          <Box>
+            Comunidades
+          </Box>
+        </div>
+      </MainGrid>
+    </>
   )
 }
